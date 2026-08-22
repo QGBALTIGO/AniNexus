b951e8c444e7b35725751f575f5e0d3f1caa407a
QGBALTIGO <119024696+QGBALTIGO@users.noreply.github.com>
2026-08-22 09:48:58 -0400

    chore(router): bump navigation to V34

--- preview-v27/navigation-v27.js
+++ preview-v27/navigation-v27.js
@@ -8,7 +8,7 @@
 
   const IS_PAGES=location.hostname.endsWith('github.io');
   const BASE=IS_PAGES?'/AniNexus':'';
-  const BUILD='33.0.0';
+  const BUILD='34.0.0';
 
   const aliases=new Map([
     ['/animes','/animes/catalogo'],
