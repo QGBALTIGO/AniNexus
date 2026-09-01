a34f5e55bcdae906e212808b80b2c3cd1035d33e
Kayky <baltigooficial@gmail.com>
2026-09-01 17:17:08 -0400

    feat: reorganizar navegacao global

--- preview-v23/detail-v23.js
+++ preview-v23/detail-v23.js
@@ -7,7 +7,7 @@
   try{
     const IS_PAGES=location.hostname.endsWith('github.io');
     const BASE=IS_PAGES?'/AniNexus':'';
-  const BUILD='42.1.6';
+  const BUILD='43.0.0';
     const u=new URL(location.href);
     const restored=u.searchParams.get('p');
     let path=window.__NX_DETAIL_V23_BOOT__||restored?.split('?')[0]||u.pathname;
